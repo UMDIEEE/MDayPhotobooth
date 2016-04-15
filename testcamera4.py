2af842d544348a01bb1979c12bd6cc4e8dbbd714
Albert Huang <alberthdev@users.noreply.github.com>
2016-04-15 02:16:56 -0400

    Remove old files

--- testcamera4.py
+++
@@ -1,3 +0,0 @@
-import camera4
-files = ['imgs/image_new.jpg', 'imgs/image2_new.jpg', 'imgs/image3_new.jpg', 'imgs/image3_new.jpg', 'imgs/image_new.jpg', 'imgs/image2_new.jpg', 'imgs/image3_new.jpg', 'imgs/image3_new.jpg' ]
-camera4.drawOverlayImage(files, 7)
--- testscam4.py
+++
@@ -1,3 +0,0 @@
-import strong_cam4
-files = ['imgs/image_new.jpg', 'imgs/image2_new.jpg', 'imgs/image3_new.jpg', 'imgs/image3_new.jpg', 'imgs/image_new.jpg', 'imgs/image2_new.jpg', 'imgs/image3_new.jpg', 'imgs/image3_new.jpg' ]
-strong_cam4.drawOverlayImage(files, 4, "output.png")
